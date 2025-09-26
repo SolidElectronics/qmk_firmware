a429bf52e85a8036accd11b9c11e9fd09e6fb565
Paul Hobbs <git@solidelectronics.net>
2025-09-26 00:19:20 -0400

    Stripped down to get encoder backlight control working

--- keyboards/logitech/g11/config.h
+++ keyboards/logitech/g11/config.h
@@ -37,19 +37,11 @@
 // With a custom matrix scan, we don't need to declare pin numbers, we can just use NO_PIN and let the matrix code return the correct struct.
 // See keyboards\keychron\v6\matrix.c example using 74HC595.
 // See keyboards\handwired\dqz11n1g\matrix.c example using 74HC165.
-#define MATRIX_ROWS 24
-#define MATRIX_COLS 32
+//#define MATRIX_ROWS 24
+//#define MATRIX_COLS 32
 
-#define DYNAMIC_KEYMAP_MACRO_COUNT 64
+//#define DYNAMIC_KEYMAP_MACRO_COUNT 64
 
-#define LED_NUM_LOCK_PIN    GP8
-#define LED_CAPS_LOCK_PIN   GP9
-#define LED_SCROLL_LOCK_PIN GP10
+//#define BACKLIGHT_PIN       GP12
 
-#define DIP_SWITCH_PINS GP13
-
-// 24-bit shift register mask
-// #define SHR_MASK { 0x000001, 0x000002, 0x000004, 0x000008, 0x000010, 0x000020, 0x000040, 0x000080, 0x000100, 0x000200, 0x000400, 0x000800, 0x001000, 0x002000, 0x004000, 0x008000, 0x010000, 0x020000, 0x040000, 0x080000, 0x100000, 0x200000, 0x400000, 0x800000}
-
-//#define ENCODER_A_PINS { GP6 }
-//#define ENCODER_B_PINS { GP7 }
+//#define ENCODER_DEFAULT_POS 0x3
--- keyboards/logitech/g11/keymaps/default/keymap.c
+++ keyboards/logitech/g11/keymaps/default/keymap.c
@@ -8,108 +8,62 @@
 // TODO: Write program in arduino to scan matrix and output codes, load that into keyboard.json.
 
 #include QMK_KEYBOARD_H
+#include "action.h"
 
-// Declare macros as custom keycodes, otherwise there is a limit of 32 QK_MACRO_## codes.
-enum custom_keycodes {
-    G11_M19  = SAFE_RANGE,
-    G11_M20,
-    G11_M21,
-    G11_M22,
-    G11_M23,
-    G11_M24,
-    G11_M25,
-    G11_M26,
-    G11_M27,
-    G11_M28,
-    G11_M29,
-    G11_M30,
-    G11_M31,
-    G11_M32,
-    G11_M33,
-    G11_M34,
-    G11_M35,
-    G11_M36,
-    G11_M37,
-    G11_M38,
-    G11_M39,
-    G11_M40,
-    G11_M41,
-    G11_M42,
-    G11_M43,
-    G11_M44,
-    G11_M45,
-    G11_M46,
-    G11_M47,
-    G11_M48,
-    G11_M49,
-    G11_M50,
-    G11_M51,
-    G11_M52,
-    G11_M53,
-    G11_M54
+#define HW_SW_LAYER     GP13
+#define HW_IND_M1       GP14
+#define HW_IND_M2       GP15
+#define HW_IND_M3       GP26
+#define HW_IND_MR       GP27
+
+void backlight_increase(void);
+void backlight_decrease(void);
+
+
+enum layers{
+  _LAYER0
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    /*
-     * [M1] [M2] [M3] [MR]                        [Fwd|Back|Stop|Play]      [Mute] [Backlight]
-     * ┌───┬───┬───┐
-     * |G1 |G2 |G3 | ┌───┐   ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┬───┐ ┌───┬───┬───┐
-     * ├───┼───┼───┤ │Esc│   │F1 │F2 │F3 │F4 │ │F5 │F6 │F7 │F8 │ │F9 │F10│F11│F12│ │PSc│Scr│Pse│
-     * |G4 |G5 |G6 | └───┘   └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┴───┘ └───┴───┴───┘
-     * └───┴───┴───┘ ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐ ┌───┬───┬───┐ ┌───┬───┬───┬───┐
-     * ┌───┬───┬───┐ │ ` │ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 0 │ - │ = │ Backsp│ │Ins│Hom│PgU│ │Num│ / │ * │ - │
-     * |G7 |G8 |G9 | ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤ ├───┼───┼───┤ ├───┼───┼───┼───┤
-     * ├───┼───┼───┤ │ Tab │ Q │ W │ E │ R │ T │ Y │ U │ I │ O │ P │ [ │ ] │  \  │ │Del│End│PgD│ │ 7 │ 8 │ 9 │   │
-     * |G10|G11|G12| ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤ └───┴───┴───┘ ├───┼───┼───┤ + │
-     * └───┴───┴───┘ │ Caps │ A │ S │ D │ F │ G │ H │ J │ K │ L │ ; │ ' │  Enter │               │ 4 │ 5 │ 6 │   │
-     * ┌───┬───┬───┐ ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────────┤     ┌───┐     ├───┼───┼───┼───┤
-     * |G13|G14|G15| │ Shift  │ Z │ X │ C │ V │ B │ N │ M │ , │ . │ / │    Shift │     │ ↑ │     │ 1 │ 2 │ 3 │   │
-     * ├───┼───┼───┤ ├────┬───┴┬──┴─┬─┴───┴───┴───┴───┴───┴──┬┴───┼───┴┬────┬────┤ ┌───┼───┼───┐ ├───┴───┼───┤Ent│
-     * |G16|G17|G18| │Ctrl│GUI │Alt │                        │ Alt│ GUI│Menu│Ctrl│ │ ← │ ↓ │ → │ │   0   │ . │   │
-     * └───┴───┴───┘ └────┴────┴────┴────────────────────────┴────┴────┴────┴────┘ └───┴───┴───┘ └───────┴───┴───┘
-     * ANSI 104 + 18 G-keys + 10 other functions = 132 keys
-     */
-    [0] = LAYOUT_ansi_104(
-        _______,     _______,     _______,      _______,                                                       KC_MNXT, KC_MPRV, KC_MSTP, KC_MPLY,                   KC_MUTE,    BL_STEP,
-        QK_MACRO_1,  QK_MACRO_2,  QK_MACRO_3,   KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
-        QK_MACRO_4,  QK_MACRO_5,  QK_MACRO_6,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        QK_MACRO_7,  QK_MACRO_8,  QK_MACRO_9,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        QK_MACRO_10, QK_MACRO_11, QK_MACRO_12,  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
-        QK_MACRO_13, QK_MACRO_14, QK_MACRO_15,  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        QK_MACRO_16, QK_MACRO_17, QK_MACRO_18,  KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
-    ),
-    [1] = LAYOUT_ansi_104(
-        _______,     _______,     _______,      _______,                                                       KC_MNXT, KC_MPRV, KC_MSTP, KC_MPLY,                   KC_MUTE,    BL_STEP,
-        G11_M19,     G11_M20,     G11_M21,      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
-        G11_M22,     G11_M23,     G11_M24,      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        G11_M25,     G11_M26,     G11_M27,      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        G11_M28,     G11_M29,     G11_M30,      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
-        G11_M31,     G11_M32,     G11_M33,      KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        G11_M34,     G11_M35,     G11_M36,      KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
-    ),
-    [2] = LAYOUT_ansi_104(
-        _______,     _______,     _______,      _______,                                                       KC_MNXT, KC_MPRV, KC_MSTP, KC_MPLY,                   KC_MUTE,    BL_STEP,
-        G11_M37,     G11_M38,     G11_M39,      KC_ESC,           KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,     KC_PSCR, KC_SCRL, KC_PAUS,
-        G11_M40,     G11_M41,     G11_M42,      KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,    KC_INS,  KC_HOME, KC_PGUP,    KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
-        G11_M43,     G11_M44,     G11_M45,      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,    KC_DEL,  KC_END,  KC_PGDN,    KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
-        G11_M46,     G11_M47,     G11_M48,      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                                   KC_P4,   KC_P5,   KC_P6,
-        G11_M49,     G11_M50,     G11_M51,      KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,             KC_UP,               KC_P1,   KC_P2,   KC_P3,   KC_PENT,
-        G11_M52,     G11_M53,     G11_M54,      KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,    KC_LEFT, KC_DOWN, KC_RGHT,    KC_P0,            KC_PDOT
-    )
+    [_LAYER0] = LAYOUT(
+		BL_UP, BL_DOWN
+	/*),
+	[_LAYER1] = LAYOUT(
+		BL_TOGG, BL_STEP
+	),
+	[_LAYER2] = LAYOUT(
+		KC_MPRV, KC_MPLY*/
+	)
 };
 
 
-#if defined(ENCODER_MAP_ENABLE)
-const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
-    //[0] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [0] = {ENCODER_CCW_CW(QK_BACKLIGHT_UP, QK_BACKLIGHT_DOWN)},
-    [1] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)},
-    [2] = {ENCODER_CCW_CW(KC_VOLD, KC_VOLU)}
-};
-#endif // ENCODER_MAP_ENABLE
+bool encoder_update_user(uint8_t index, bool clockwise) {
+    if (index == 0) { // First encoder
+        if (clockwise) {
+            //tap_code(KC_0 + ((BL_UP >> 12) & 0xF));
+            backlight_increase();
+        } else {
+            //tap_code(KC_0 + ((BL_DOWN >> 12) & 0xF));
+            backlight_decrease();
+        }
+    }
+    return false;
+}
 
-#if defined(DIP_SWITCH_MAP_ENABLE)
-const uint16_t PROGMEM dip_switch_map[NUM_DIP_SWITCHES][NUM_DIP_STATES] = {
-    DIP_SWITCH_OFF_ON(DF(0), DF(1))
-};
-#endif
+
+/*
+// Scan routine
+void matrix_scan_user(void) {
+    static bool last_layer_switch = false;
+    bool layer_switch = !readPin(HW_SW_LAYER);
+
+    if (layer_switch != last_layer_switch) {
+        if (layer_switch) {
+            layer_on(1);
+        } else {
+            layer_off(1);
+        }
+        last_layer_switch = layer_switch;
+    }
+}
+*/
