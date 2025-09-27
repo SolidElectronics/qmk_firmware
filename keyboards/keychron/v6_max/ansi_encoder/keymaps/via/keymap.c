6db492678c81d4795dae6da5cabe0928391dd6b9
Paul Hobbs <git@solidelectronics.net>
2025-09-26 22:51:52 -0400

    Fix custom animation being full brightness

--- keyboards/keychron/v6_max/ansi_encoder/keymaps/via/keymap.c
+++ keyboards/keychron/v6_max/ansi_encoder/keymaps/via/keymap.c
@@ -30,39 +30,6 @@ enum jiggle_keycode {
 };
 
 // clang-format off
-/*
-// FACTORY DEFAULT LAYOUT
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [MAC_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_BRID,  KC_BRIU,  KC_MCTRL, KC_LNPAD, RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    KC_MUTE,    KC_SNAP,  KC_SIRI,  RGB_MOD,  KC_F13,   KC_F14,   KC_F15,   KC_F16,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LOPTN, KC_LCMMD,                               KC_SPC,                                 KC_RCMMD, KC_ROPTN, MO(MAC_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
-    [MAC_FN] = LAYOUT_ansi_109(
-        _______,  KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     RGB_TOG,    _______,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  _______,  _______,  _______,
-        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______),
-    [WIN_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,     KC_MUTE,    KC_PSCR,  KC_CTANA, RGB_MOD,  _______,  _______,  _______,  _______,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,   KC_PSLS,  KC_PAST,  KC_PMNS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,    KC_P8,    KC_P9,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,              KC_ENT,                                   KC_P4,    KC_P5,    KC_P6,    KC_PPLS,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,              KC_RSFT,              KC_UP,              KC_P1,    KC_P2,    KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,              KC_PDOT,  KC_PENT),
-    [WIN_FN] = LAYOUT_ansi_109(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,    RGB_TOG,    _______,  _______,  RGB_TOG,  _______,  _______,  _______,  _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,  _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,  _______,  _______,
-        _______,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,              _______,                                  _______,  _______,  _______,  _______,
-        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,              _______,              _______,            _______,  _______,  _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______)
-};
-*/
 
 /*
 WIN_BASE:  Standard 104 layout
@@ -72,7 +39,7 @@ WIN_FN:    Secondary action on function keys
            Bluetooth profile selection on 1-3
            RGB controls: Q/A: Mode, W/S: Brightness, E/D: Hue, R/F: Sat, T/G: Speed, Tab: on/off
 		   Wheel operates RGB brightness, click on/off
-WIN_MACRO: Macro keys on numpad 1-9
+MACRO1:    Macro keys on numpad 1-9
            Mouse jiggle on F13
 		   Wheel operates RGB speed
 */
@@ -95,28 +62,28 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         _______,  _______,  _______,                                _______,                                _______,  _______,  _______,    _______,    _______,  _______,  _______,  _______,            _______,  _______
 	),
     [WIN_BASE] = LAYOUT_ansi_109(
-        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,   KC_F12,            KC_MUTE,    KC_PSCR,  KC_SCRL,  KC_BRK,   QK_MACRO_10, QK_MACRO_11, QK_MACRO_12, QK_MACRO_13,
-        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,  KC_EQL,            KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,      KC_PSLS,     KC_PAST,     KC_PMNS,
-        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,  KC_RBRC,           KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,       KC_P8,       KC_P9,
-        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,                     KC_ENT,                                   KC_P4,       KC_P5,       KC_P6,       KC_PPLS,
-        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,                     KC_RSFT,              KC_UP,              KC_P1,       KC_P2,       KC_P3,
-        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_RWIN,  LT(WIN_FN,KC_APP), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,                    KC_PDOT,     KC_PENT
+        KC_ESC,   KC_F1,    KC_F2,    KC_F3,    KC_F4,    KC_F5,    KC_F6,    KC_F7,    KC_F8,    KC_F9,    KC_F10,   KC_F11,    KC_F12,     KC_MUTE,    KC_PSCR,  KC_SCRL,  KC_BRK,   QK_MACRO_10, QK_MACRO_11, QK_MACRO_12, QK_MACRO_13,
+        KC_GRV,   KC_1,     KC_2,     KC_3,     KC_4,     KC_5,     KC_6,     KC_7,     KC_8,     KC_9,     KC_0,     KC_MINS,   KC_EQL,     KC_BSPC,    KC_INS,   KC_HOME,  KC_PGUP,  KC_NUM,      KC_PSLS,     KC_PAST,     KC_PMNS,
+        KC_TAB,   KC_Q,     KC_W,     KC_E,     KC_R,     KC_T,     KC_Y,     KC_U,     KC_I,     KC_O,     KC_P,     KC_LBRC,   KC_RBRC,    KC_BSLS,    KC_DEL,   KC_END,   KC_PGDN,  KC_P7,       KC_P8,       KC_P9,
+        KC_CAPS,  KC_A,     KC_S,     KC_D,     KC_F,     KC_G,     KC_H,     KC_J,     KC_K,     KC_L,     KC_SCLN,  KC_QUOT,               KC_ENT,                                   KC_P4,       KC_P5,       KC_P6,       KC_PPLS,
+        KC_LSFT,            KC_Z,     KC_X,     KC_C,     KC_V,     KC_B,     KC_N,     KC_M,     KC_COMM,  KC_DOT,   KC_SLSH,               KC_RSFT,              KC_UP,              KC_P1,       KC_P2,       KC_P3,
+        KC_LCTL,  KC_LWIN,  KC_LALT,                                KC_SPC,                                 KC_RALT,  KC_APP,    MO(WIN_FN), KC_RCTL,    KC_LEFT,  KC_DOWN,  KC_RGHT,  KC_P0,                    KC_PDOT,     KC_PENT
 	),
     [WIN_FN] = LAYOUT_ansi_109(
-        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,  KC_VOLU,           RGB_TOG,    _______,  KC_SCRL,  _______,  _______,     _______,     _______,     _______,
-        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,    _______,  _______,  _______,  KC_NUM,      _______,     _______,     _______,
-        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,    _______,  _______,  _______,  _______,     _______,     _______,
-        KC_CAPS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,                     _______,                                  _______,     _______,     _______,     _______,
-        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,                     _______,              _______,            _______,     _______,     _______,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  KC_TRNS,           MO(MACRO1), _______,  _______,  _______,  _______,                  _______,     _______
+        _______,  KC_BRID,  KC_BRIU,  KC_TASK,  KC_FILE,  RGB_VAD,  RGB_VAI,  KC_MPRV,  KC_MPLY,  KC_MNXT,  KC_MUTE,  KC_VOLD,   KC_VOLU,    RGB_TOG,    _______,  KC_SCRL,  _______,  _______,     _______,     _______,     _______,
+        _______,  BT_HST1,  BT_HST2,  BT_HST3,  P2P4G,    _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,    _______,    _______,  _______,  _______,  KC_NUM,      _______,     _______,     _______,
+        RGB_TOG,  RGB_MOD,  RGB_VAI,  RGB_HUI,  RGB_SAI,  RGB_SPI,  _______,  _______,  _______,  _______,  _______,  _______,   _______,    _______,    _______,  _______,  _______,  _______,     _______,     _______,
+        KC_CAPS,  RGB_RMOD, RGB_VAD,  RGB_HUD,  RGB_SAD,  RGB_SPD,  _______,  _______,  _______,  _______,  _______,  _______,               _______,                                  _______,     _______,     _______,     _______,
+        _______,            _______,  _______,  _______,  _______,  BAT_LVL,  NK_TOGG,  _______,  _______,  _______,  _______,               _______,              _______,            _______,     _______,     _______,
+        _______,  _______,  _______,                                _______,                                _______,  MO(MACRO1),KC_TRNS,    KC_TRNS,    _______,  _______,  _______,  _______,                  _______,     _______
 	),
 	[MACRO1] = LAYOUT_ansi_109(
-		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,    _______,  _______,  _______,  M_JIGL,      _______,     _______,     QK_BOOT,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,    _______,  _______,  _______,  _______,     _______,     _______,     _______,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,           _______,    _______,  _______,  _______,  QK_MACRO_7,  QK_MACRO_8,  QK_MACRO_9,
-        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                     _______,                                  QK_MACRO_4,  QK_MACRO_5,  QK_MACRO_6,  _______,
-        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,                     _______,              _______,            QK_MACRO_1,  QK_MACRO_2,  QK_MACRO_3,
-        _______,  _______,  _______,                                _______,                                _______,  _______,  KC_TRNS,           KC_TRNS,    _______,  _______,  _______,  QK_MACRO_0,               _______,     _______
+		_______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,    _______,    _______,  KC_SCRL,  _______,  M_JIGL,      _______,     _______,     QK_BOOT,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,    _______,    _______,  _______,  _______,  KC_NUM,      _______,     _______,     _______,
+        _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,   _______,    _______,    _______,  _______,  _______,  QK_MACRO_7,  QK_MACRO_8,  QK_MACRO_9,
+        KC_CAPS,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,               _______,                                  QK_MACRO_4,  QK_MACRO_5,  QK_MACRO_6,  _______,
+        _______,            _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,  _______,               _______,              _______,            QK_MACRO_1,  QK_MACRO_2,  QK_MACRO_3,
+        _______,  _______,  _______,                                _______,                                _______,  _______,   KC_TRNS,    KC_TRNS,    _______,  _______,  _______,  QK_MACRO_0,               _______,     _______
 	)
 };
 
@@ -194,21 +161,21 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
                         }
                         break;
                     case KC_NUM:
-                        if (current_layer == WIN_FN) {
+                        if (current_layer >= WIN_FN) {
                             (host_keyboard_led_state().num_lock) ? rgb_matrix_set_color(index, RGB_WHITE) : rgb_matrix_set_color(index, RGB_BLACK);
                         } else {
                             if (! host_keyboard_led_state().num_lock) { rgb_matrix_set_color(index, RGB_BLACK); }
                         }
                         break;
                     case KC_CAPS:
-                        if (current_layer == WIN_FN) {
+                        if (current_layer >= WIN_FN) {
                             (host_keyboard_led_state().caps_lock) ? rgb_matrix_set_color(index, RGB_WHITE) : rgb_matrix_set_color(index, RGB_BLACK);
                         } else {
                             if (! host_keyboard_led_state().caps_lock) { rgb_matrix_set_color(index, RGB_BLACK); }
                         }
                         break;
                     case KC_SCRL:
-                        if (current_layer == WIN_FN) {
+                        if (current_layer >= WIN_FN) {
                             (host_keyboard_led_state().scroll_lock) ? rgb_matrix_set_color(index, RGB_WHITE) : rgb_matrix_set_color(index, RGB_BLACK);
                         } else {
                             if (! host_keyboard_led_state().scroll_lock) { rgb_matrix_set_color(index, RGB_BLACK); }
