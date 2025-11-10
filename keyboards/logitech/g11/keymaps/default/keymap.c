5ea3cca68c8622fc0ee7b47eb8df4e2b83247992
Paul Hobbs <git@solidelectronics.net>
2025-11-10 00:34:12 -0500

    Keys working

--- keyboards/logitech/g11/keymaps/default/keymap.c
+++ keyboards/logitech/g11/keymaps/default/keymap.c
@@ -17,9 +17,9 @@
 #define HW_IND_MR       GP13
 
 enum layers{
-  _LAYER0
-  //_LAYER1,
-  //_LAYER2
+  _LAYER0,
+  _LAYER1,
+  _LAYER2
 };
 
 // This is a forward declaration.  These functions exist, but aren't declared when this compiles.  This lets the compiler ignore that fact and fix it during linking.
@@ -218,33 +218,32 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     )
 };
 */
-/*
-// Test definitions
+
     [_LAYER0] = LAYOUT(
-		BL_UP, BL_DOWN
-	),
-	[_LAYER1] = LAYOUT(
-		BL_TOGG, BL_STEP
-	),
-	[_LAYER2] = LAYOUT(
-		KC_MPRV, KC_MPLY
-	)
-};
-*/
-/*
-    [_LAYER0] = LAYOUT(
-        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,
-        KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,
-        KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,
-        KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM,
-        KC_ESC,  KC_TAB,  KC_LSFT, KC_LCTL, KC_LALT, KC_SPC,  KC_ENT,  KC_BSPC,
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,
-        KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_HOME, KC_END,  KC_PGUP, KC_PGDN,
-        KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_P4,   KC_P5,   KC_P6,   KC_P7
-    )
-*/
-    [_LAYER0] = LAYOUT(
-        KC_A,       KC_B,   KC_C,   KC_D,   KC_MUTE,    BL_TOGG,
-        QK_BOOT,    KC_1,   KC_2
+                     KC_NO,       TO(1),        QK_BOOT,                                     KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY,                            KC_MUTE, BL_STEP,
+        QK_MACRO_1,  QK_MACRO_2,  QK_MACRO_3,   KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_PSCR,  KC_SCRL, KC_PAUS,
+        QK_MACRO_4,  QK_MACRO_5,  QK_MACRO_6,   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,  KC_INS,  KC_HOME, KC_PGUP,  KC_NUM,  KC_PSLS, KC_PAST, KC_PMNS,
+        QK_MACRO_7,  QK_MACRO_8,  QK_MACRO_9,   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,  KC_DEL,  KC_END,  KC_PGDN,  KC_P7,   KC_P8,   KC_P9,   KC_PPLS,
+        QK_MACRO_10, QK_MACRO_11, QK_MACRO_12,  KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                               KC_P4,   KC_P5,   KC_P6,
+        QK_MACRO_13, QK_MACRO_14, QK_MACRO_15,  KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,           KC_UP,             KC_P1,   KC_P2,   KC_P3,   KC_PENT,
+        QK_MACRO_16, QK_MACRO_17, QK_MACRO_18,  KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, KC_RGUI, KC_APP,  KC_RCTL,  KC_LEFT, KC_DOWN, KC_RGHT,  KC_P0,            KC_PDOT
+    ),
+    [_LAYER1] = LAYOUT(
+                     _______,     TO(2),        _______,                                     _______, _______, _______, _______,                            _______, BL_TOGG,
+        G11_M19,     G11_M20,     G11_M21,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        G11_M22,     G11_M23,     G11_M24,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        G11_M25,     G11_M26,     G11_M27,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        G11_M28,     G11_M29,     G11_M30,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                            _______, _______, _______,
+        G11_M31,     G11_M32,     G11_M33,      _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,          _______, _______, _______, _______,
+        G11_M34,     G11_M35,     G11_M36,      _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______, _______, _______,          _______
+    ),
+    [_LAYER2] = LAYOUT(
+                     _______,     TO(0),        _______,                                     _______, _______, _______, _______,                            _______, _______,
+        G11_M37,     G11_M38,     G11_M39,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        G11_M40,     G11_M41,     G11_M42,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        G11_M43,     G11_M44,     G11_M45,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
+        G11_M46,     G11_M47,     G11_M48,      _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,                            _______, _______, _______,
+        G11_M49,     G11_M50,     G11_M51,      _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,          _______,          _______, _______, _______, _______,
+        G11_M52,     G11_M53,     G11_M54,      _______, _______, _______,                            _______,                            _______, _______, _______, _______, _______, _______, _______, _______,          _______
     )
 };
--- keyboards/logitech/g11/matrix.c
+++ keyboards/logitech/g11/matrix.c
@@ -28,388 +28,96 @@ void matrix_init_custom(void) {
     writePinLow(SW_B_PIN);
 }
 
-// Activate row pin
-static void shift_out_rows(uint8_t data) {
-    // Shift out data all rows - round down to multiples of 8
-    for (int i = 0; i <= (MATRIX_ROWS & ~7); i++) {
-        writePinLow(ROW_CLOCK_PIN);
 
-        if (data == i) {
+// Write 24 bits to the 74HC595 shift registers to select a row
+static void select_row(uint8_t row) {
+    // Rows 24-25 are on invidual MCU pins.  For those, we can still do the shift register to zero it out.
+    if (row == 24) {
+        writePinHigh(SW_A_PIN);
+        writePinLow(SW_B_PIN);
+    } else if (row == 25) {
+        writePinLow(SW_A_PIN);
+        writePinHigh(SW_B_PIN);
+    } else {
+        writePinLow(SW_A_PIN);
+        writePinLow(SW_B_PIN);
+    }
+
+    // We'll send 24 bits (3 bytes) with only one bit HIGH
+    // The bit position corresponds to the row number
+
+    writePinLow(ROW_LATCH_PIN);  // Prepare to shift data
+
+    // Send 24 bits, MSB first
+    for (int8_t i = 23; i >= 0; i--) {
+        // Set data pin HIGH only for the selected row
+        if (i == row) {
             writePinHigh(ROW_DATA_PIN);
         } else {
             writePinLow(ROW_DATA_PIN);
         }
 
+        // Clock the bit in
         writePinHigh(ROW_CLOCK_PIN);
-    }
-
-    // Latch the data to outputs
-    writePinLow(ROW_LATCH_PIN);
-    writePinHigh(ROW_LATCH_PIN);
-}
-
-// Zero all row pins
-static void unselect_rows(void) {
-    writePinLow(SW_A_PIN);
-    writePinLow(SW_B_PIN);
-
-    // Shift out data all rows
-    for (int i = 0; i <= (MATRIX_ROWS & ~7); i++) {
+        wait_us(1);
         writePinLow(ROW_CLOCK_PIN);
-        writePinLow(ROW_DATA_PIN);
-        writePinHigh(ROW_CLOCK_PIN);
+        wait_us(1);
     }
 
-    // Latch the data to outputs
-    writePinLow(ROW_LATCH_PIN);
+    // Latch the data to the output pins
     writePinHigh(ROW_LATCH_PIN);
+    wait_us(1);
+    writePinLow(ROW_LATCH_PIN);
+    wait_us(5);  // Give the row line time to stabilize
 }
 
-bool matrix_read_cols_on_row(matrix_row_t current_matrix[], uint8_t current_row) {
-    /* Start with a clear matrix row */
-    matrix_row_t current_row_value = 0;
 
-    // Select row
-    // Rows 0-23 are via shift registers
-    // Rows 24-25 are pins on MCU
-    if (current_row <= 23) {
-        writePinLow(SW_A_PIN);
-        writePinLow(SW_B_PIN);
-        shift_out_rows(current_row);
-    }
-    else if (current_row == 24) {
-        unselect_rows();
-        writePinHigh(SW_A_PIN);
-    }
-    else if (current_row == 25) {
-        unselect_rows();
-        writePinHigh(SW_B_PIN);
-    }
+// Read 32 bits from the 74HC165 shift registers (column states)
+static matrix_row_t read_cols(void) {
+    matrix_row_t cols = 0;
 
-    // Trigger parallel load of column data (active low pulse)
+    // Trigger parallel load - capture current state of all inputs
     writePinLow(COL_SH_PIN);
     wait_us(1);
     writePinHigh(COL_SH_PIN);
     wait_us(1);
 
-    // Unselect row
-    unselect_rows();
+    // Read 32 bits serially (4 shift registers × 8 bits)
+    for (int i = 0; i < 32; i++) {
+        // Read the current bit and shift it into position
+        // Reading MSB first (bit 31 down to bit 0)
+        cols |= (readPin(COL_DATA_PIN) ? 1UL : 0UL) << (31 - i);
 
-    // Read column data for this row
-    for (uint8_t col_index = 0; col_index < MATRIX_COLS; col_index ++) {
-
-        // Pulse clock
+        // Clock to shift next bit
         writePinHigh(COL_CLK_PIN);
         wait_us(1);
         writePinLow(COL_CLK_PIN);
         wait_us(1);
-
-        //uprintf("%d", (readPin(COL_DATA_PIN) ? 1 : 0));
-
-        current_row_value |= (readPin(COL_DATA_PIN) ? 1 : 0) << (MATRIX_COLS - col_index);
     }
-    //uprintf(")\n");
 
-    // Update row in matrix
-    if (current_row_value != current_matrix[current_row]) {
-        current_matrix[current_row] = current_row_value;
-        return true;
-    }
-    return false;
+    return cols;
 }
 
 
 bool matrix_scan_custom(matrix_row_t current_matrix[]) {
     bool changed = false;
 
-    //wait_ms(100);
-
     /* set row, read cols */
-    for (uint8_t current_row = 0; current_row < MATRIX_ROWS; current_row++) {
-        matrix_row_t new_row_data = read_row(current_row);
-        if (current_matrix[row] != new_row_data) {
-            current_matrix[row] = new_row_data;
+    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
+        select_row(row);
+        wait_us(10);
+        matrix_row_t cols = read_cols();
+
+        if (current_matrix[row] != cols) {
+            current_matrix[row] = cols;
             changed = true;
         }
 
         //changed |= matrix_read_cols_on_row(curr_matrix, current_row);
     }
 
-    return changed;
-}
-
-/*
-static void shift_out_rows(uint8_t data) {
-    for (int i = 7; i >= 0; i--) {
-        writePinLow(ROW_CLOCK_PIN);
-
-        if (data & (1 << i)) {
-            writePinHigh(ROW_DATA_PIN);
-        } else {
-            writePinLow(ROW_DATA_PIN);
-        }
-
-        writePinHigh(ROW_CLOCK_PIN);
-    }
-
-    // Latch the data to outputs
-    writePinLow(ROW_LATCH_PIN);
-    writePinHigh(ROW_LATCH_PIN);
-}
-
-
-// Shift in data from 74HC165 (columns)
-static uint8_t shift_in_cols(void) {
-    uint8_t data = 0;
-
-    // Trigger parallel load (active low pulse)
-    writePinLow(COL_SH_PIN);
-    wait_us(1);
-    writePinHigh(COL_SH_PIN);
-    wait_us(1);
-
-    // Shift in 8 bits
-    for (int i = 0; i < 8; i++) {
-        data |= (readPin(COL_DATA_PIN) ? 1 : 0) << (7 - i);
-
-        writePinHigh(COL_CLK_PIN);
-        wait_us(1);
-        writePinLow(COL_CLK_PIN);
-        wait_us(1);
-    }
-
-    return data;
-}
-
-
-bool matrix_scan_custom(matrix_row_t current_matrix[]) {
-    matrix_row_t curr_matrix[MATRIX_ROWS] = {0};
-
-    for (uint8_t row = 0; row < MATRIX_ROWS; row++) {
-        // Select current row (set bit high for active row)
-        shift_out_rows(1 << row);
-
-        // Small delay for signal stabilization
-        wait_us(10);
-
-        // Read column states
-        curr_matrix[row] = shift_in_cols();
-
-        // Print what we're reading
-        uprintf("Row %d: 0x%02X (binary: ", row, curr_matrix[row]);
-        for (int i = 7; i >= 0; i--) {
-            uprintf("%d", (curr_matrix[row] >> i) & 1);
-        }
-        uprintf(")\n");
-    }
-
-    // Deselect all rows
-    shift_out_rows(0x00);
-
-    // Check if matrix changed and update if needed
-    bool changed = memcmp(current_matrix, curr_matrix, sizeof(curr_matrix)) != 0;
-    if (changed) {
-        memcpy(current_matrix, curr_matrix, sizeof(curr_matrix));
-        uprintf("Matrix changed!\n");
-    }
-    print("Matrix scanned\n");
-    wait_ms(250);
-    return changed;
-}
-*/
-
-
-/*
-Matrix rows being driven by 74HC595 (parallel-out shift register)
-Column rows being read by 74HC165 (parallel-in shift register)
-*/
-/*
-#ifdef MATRIX_ROW_PINS
-static pin_t row_pins[MATRIX_ROWS] = MATRIX_ROW_PINS;
-#endif // MATRIX_ROW_PINS
-#ifdef MATRIX_COL_PINS
-static pin_t col_pins[MATRIX_COLS] = MATRIX_COL_PINS;
-#endif // MATRIX_COL_PINS
-
-
-static inline void gpio_atomic_set_pin_output_low(pin_t pin) {
-    ATOMIC_BLOCK_FORCEON {
-        gpio_set_pin_output(pin);
-        gpio_write_pin_low(pin);
-    }
-}
-
-static inline void gpio_atomic_set_pin_output_high(pin_t pin) {
-    ATOMIC_BLOCK_FORCEON {
-        gpio_set_pin_output(pin);
-        gpio_write_pin_high(pin);
-    }
-}
-
-static inline void gpio_atomic_set_pin_input_high(pin_t pin) {
-    ATOMIC_BLOCK_FORCEON {
-        gpio_set_pin_input_high(pin);
-    }
-}
-
-static inline uint8_t readMatrixPin(pin_t pin) {
-    if (pin != NO_PIN) {
-        return gpio_read_pin(pin);
-    } else {
-        return 1;
-    }
-}
-
-void small_delay(volatile uint8_t timeout) {
-    while (timeout--);
-}
-
-static void shiftOut(uint16_t dataOut) {
-    ATOMIC_BLOCK_FORCEON {
-        for (uint8_t i = 0; i < MATRIX_ROWS; i++) {
-            if (dataOut & 0x1) {
-                gpio_write_pin_high(DATA_PIN_74HC595);
-            } else {
-                gpio_write_pin_low(DATA_PIN_74HC595);
-            }
-            dataOut = dataOut >> 1;
-            gpio_write_pin_high(CLOCK_PIN_74HC595);
-            small_delay(2);
-            gpio_write_pin_low(CLOCK_PIN_74HC595);
-        }
-        gpio_write_pin_high(LATCH_PIN_74HC595);
-        small_delay(2);
-        gpio_write_pin_low(LATCH_PIN_74HC595);
-    }
-}
-
-static void shiftOut_single(uint8_t data) {
-    ATOMIC_BLOCK_FORCEON {
-        if (data & 0x1) {
-            gpio_write_pin_high(DATA_PIN_74HC595);
-        } else {
-            gpio_write_pin_low(DATA_PIN_74HC595);
-        }
-        gpio_write_pin_high(CLOCK_PIN_74HC595);
-        small_delay(2);
-        gpio_write_pin_low(CLOCK_PIN_74HC595);
-        gpio_write_pin_high(LATCH_PIN_74HC595);
-        small_delay(2);
-        gpio_write_pin_low(LATCH_PIN_74HC595);
-    }
-}
-
-static bool select_col(uint8_t col) {
-    pin_t pin = col_pins[col];
-
-    if (pin != NO_PIN) {
-        gpio_atomic_set_pin_output_low(pin);
-        return true;
-    } else {
-        if (col == PIN_START_74HC595) {
-            shiftOut_single(0x00);
-        }
-        return true;
-    }
-    return false;
-}
-
-static void unselect_col(uint8_t col) {
-    pin_t pin = col_pins[col];
-
-    if (pin != NO_PIN) {
-#ifdef MATRIX_UNSELECT_DRIVE_HIGH
-        gpio_atomic_set_pin_output_high(pin);
-#else
-        gpio_atomic_set_pin_input_high(pin);
-#endif
-    } else {
-        shiftOut_single(0x01);
-    }
-}
-
-static void unselect_cols(void) {
-    // unselect column pins
-    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
-        pin_t pin = col_pins[x];
-
-        if (pin != NO_PIN) {
-#ifdef MATRIX_UNSELECT_DRIVE_HIGH
-            gpio_atomic_set_pin_output_high(pin);
-#else
-            gpio_atomic_set_pin_input_high(pin);
-#endif
-        }
-        if (x == PIN_START_74HC595)
-            // unselect Shift Register
-            shiftOut(0xFFFF);
-    }
-}
-
-static void matrix_init_pins(void) {
-    gpio_set_pin_output(DATA_PIN_74HC595);
-    gpio_set_pin_output(CLOCK_PIN_74HC595);
-    gpio_set_pin_output(LATCH_PIN_74HC595);
-#ifdef MATRIX_UNSELECT_DRIVE_HIGH
-    for (uint8_t x = 0; x < MATRIX_COLS; x++) {
-        if (col_pins[x] != NO_PIN) {
-            gpio_set_pin_output(col_pins[x]);
-        }
-    }
-#endif
-    unselect_cols();
-    for (uint8_t x = 0; x < MATRIX_ROWS; x++) {
-        if (row_pins[x] != NO_PIN) {
-            gpio_atomic_set_pin_input_high(row_pins[x]);
-        }
-    }
-}
-
-static void matrix_read_rows_on_col(matrix_row_t current_matrix[], uint8_t current_col, matrix_row_t row_shifter) {
-    bool key_pressed = false;
-
-    // Select col
-    if (!select_col(current_col)) { // select col
-        return;                     // skip NO_PIN col
-    }
-
-    matrix_output_select_delay();
-
-    // For each row...
-    for (uint8_t row_index = 0; row_index < ROWS_PER_HAND; row_index++) {
-        // Check row pin state
-        if (readMatrixPin(row_pins[row_index]) == 0) {
-            // Pin LO, set col bit
-            current_matrix[row_index] |= row_shifter;
-            key_pressed = true;
-        } else {
-            // Pin HI, clear col bit
-            current_matrix[row_index] &= ~row_shifter;
-        }
-    }
-
-    // Unselect col
-    unselect_col(current_col);
-    matrix_output_unselect_delay(current_col, key_pressed); // wait for all Row signals to go HIGH
-}
-
-void matrix_init_custom(void) {
-    // initialize key pins
-    matrix_init_pins();
-}
-
-bool matrix_scan_custom(matrix_row_t current_matrix[]) {
-    matrix_row_t curr_matrix[MATRIX_ROWS] = {0};
-
-    // Set col, read rows
-    matrix_row_t row_shifter = MATRIX_ROW_SHIFTER;
-    for (uint8_t current_col = 0; current_col < MATRIX_COLS; current_col++, row_shifter <<= 1) {
-        matrix_read_rows_on_col(curr_matrix, current_col, row_shifter);
-    }
-
-    bool changed = memcmp(current_matrix, curr_matrix, sizeof(curr_matrix)) != 0;
-    if (changed) memcpy(current_matrix, curr_matrix, sizeof(curr_matrix));
+    // Release all row pins
+    select_row(0xFF);
 
     return changed;
 }
-*/
