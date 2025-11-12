323a45319a9bc739e3a13ca353dba671b63dac21
Paul Hobbs <git@solidelectronics.net>
2025-11-12 00:26:52 -0500

    Working build with Via support

--- keyboards/logitech/g11/config.h
+++ keyboards/logitech/g11/config.h
@@ -2,26 +2,26 @@
 
 /* Hardware pin definitions (RP2040 Zero)
  *
- * (Row outputs driven via 3 x 74HC595D)
+ * (24 row outputs driven via 3 x 74HC595D)
  * GPIO5: Row serial data out
  * GPIO3: Row clock
  * GPIO4: Row store
  *
- * (Column pins scanned via 4 x 74HC165)
+ * GPIO11: Media control buttons row A
+ * GPIO12: Media control buttons row B
+
+ * (32 column pins scanned via 4 x 74HC165)
  * GPIO6: Column serial data in
  * GPIO7: Column clock
  * GPIO8: Column SH (?)
  *
  * GPIO10: Rotary encoder A pin
  * GPIO9: Rotary encoder B pin
+ * GPIO27: Normal/game slider switch
  *
  * GPIO2: Num lock LED
  * GPIO1: Caps lock LED
  * GPIO0: Scroll lock LED
- *
- * GPIO11: Media control buttons column A
- * GPIO12: Media control buttons column B
- * GPIO27: Normal/game slider switch
  *
  * GPIO26: M1 indicator
  * GPIO15: M2 indicator
@@ -33,13 +33,14 @@
  */
 
 
-// This needs to match the size of the matrix_pins array in keyboard.json or it will complain.
+// This needs to match the size of the matrix_pins array in keyboard.json.
 // With a custom matrix scan, we don't need to declare pin numbers, we can just use NO_PIN and let the matrix code return the correct struct.
-// See keyboards\keychron\v6\matrix.c example using 74HC595.
-// See keyboards\handwired\dqz11n1g\matrix.c example using 74HC165.
 #define MATRIX_ROWS     26
 #define MATRIX_COLS     32
 
+#define WEAR_LEVELING_BACKING_SIZE 65536  // Max amount of memory available as flash on RP2040
+
+// Matrix pins are defined here
 #define ROW_DATA_PIN    GP5
 #define ROW_CLOCK_PIN   GP3
 #define ROW_LATCH_PIN   GP4
@@ -51,12 +52,12 @@
 #define SW_A_PIN        GP11
 #define SW_B_PIN        GP12
 
-//#define BACKLIGHT_PIN   GP28
+#define DEBUG_ENABLE    false
+#define DEBUG_MATRIX    false
 
-#define DEBUG_ENABLE    true
-#define DEBUG_MATRIX    true
-
-
+#define DYNAMIC_KEYMAP_LAYER_COUNT 3
 #define DYNAMIC_KEYMAP_MACRO_COUNT 64
+
+#define DISABLE_LEADER
 
 //#define ENCODER_DEFAULT_POS 0x3
---
+++ keyboards/logitech/g11/keymaps/default/rules.mk
@@ -0,0 +1,5 @@
+EEPROM_DRIVER = wear_leveling
+WEAR_LEVELING_DRIVER = rp2040_flash
+CUSTOM_MATRIX = lite
+CONSOLE_ENABLE = yes
+SRC += matrix.c
